85404153d18e0e015e8e9e4dfd689074c27b4205
LambdaX <joshua@btlmd.com>
2022-06-13 00:38:33 +0800

    min_clip

--- readme.md
+++ readme.md
@@ -13,12 +13,17 @@ TODO
 
 随机生成一道 Alpha-Beta 剪枝题目及其解答。
 
+生成时先生成一颗满树，然后随机删除子树，直到叶子数足够少。约束条件见配置。然后试着给树叶赋值，如果赋值 200 次后仍然剪枝次数太少，就重新生成，直到树的剪枝数足够多，同时叶子也足够少为止。
+
+（如果条件比较苛刻可能反复生成十几次才能达到要求qwq
+
+### 运行方法
 ```bash
 python tree.py
 ```
 运行后会根据 `settings.json` 中的配置，将题目和解答生成到 `img` 目录下。其中，`todo_**.jpg` 是题目，`done_**.jpg` 是解答，`**` 是生成是使用的随机数种子。
 
-运行时，输出节点数和剪枝数。如果剪枝数太少，直接重新生成即可。
+运行时，输出平均分支数，节点数和剪枝数。
 
 运行过程中输出
 ```bash
@@ -26,6 +31,18 @@ tree.py:93: RuntimeWarning: Couldn't reach some vertices at src/paths/unweighted
   path = graph.get_shortest_paths(0, nd)[0]
 ```
 是预期内的行为。
+
+### 配置说明
+
+```bash
+"max_ch": 最大分枝数
+"layers": 层数
+"max_leaves": 叶子数上限
+"min_clip": 剪枝次数下限
+"show_clip_count": 是否在stdout输出有多少剪枝
+"distinguish_max_min": 图中是否区分极大极小节点
+"save_path": 生成图片保存路径
+```
 
 ## 决策树
 
--- tree.py
+++ tree.py
@@ -6,6 +6,8 @@ import time
 import json
 from attrdict import AttrDict
 import pathlib
+import os
+from tqdm import tqdm
 
 with open("settings.json", "r") as f:
     settings = json.load(f)
@@ -15,10 +17,6 @@ with open("settings.json", "r") as f:
 sd = settings["seed"] if "seed" in settings else int(time.time())
 print("Current random seed", sd)
 random.seed(sd)
-
-n_vertices = int((settings.max_ch ** settings.layers - 1) / (settings.max_ch - 1))
-v_label = list(map(str, range(n_vertices)))
-G = Graph.Tree(n_vertices, settings.max_ch)
 
 clipped = []
 Xpos = {}
@@ -75,7 +73,6 @@ def alpha_beta(g: Graph, node: int, anc: list, is_max: bool = True):
             glob_clip = True
     return g.vs[node]["reward"]
 
-
 def leaves(graph: Graph):
     l_c = 0
     for v in range(1, len(graph.vs)):
@@ -83,59 +80,88 @@ def leaves(graph: Graph):
             l_c += 1
     return l_c
 
-
-deletions = 0
-
-
-def cascade_del(graph: Graph, node: int) -> Graph:
-    global deletions
-    assert node
-    graph = graph - node
-    deletions += 1
-
+def conditional_gen():
     while True:
-        is_del = False
-        for nd in range(len(graph.vs)):
-            is_del = False
-            path = graph.get_shortest_paths(0, nd)[0]
-            if len(path) == 0:
-                graph = graph - nd
-                deletions += 1
-                is_del = True
-                break
+        deletions = 0
+        n_vertices = int((settings.max_ch ** settings.layers - 1) / (settings.max_ch - 1))
+        _G = Graph.Tree(n_vertices, settings.max_ch)
 
-        if not is_del:
-            break
+        def cascade_del(_del, graph: Graph, node: int) -> Graph:
+            assert node
+            graph = graph - node
+            _del += 1
+            paths = graph.shortest_paths(0)[0]
+            tbd = []
+            for idx, p in enumerate(paths):
+                if isinf(p):
+                    tbd += [idx]
+            graph = graph - tbd
+            _del += len(tbd)
+            return _del, graph
 
-    return graph
+        print("Shaping...")
+        l_count = leaves(_G)
+        bar = tqdm(total=l_count-settings.max_leaves)
+        while l_count > settings.max_leaves:
+            rml = random.randint(settings.max_ch, len(_G.vs) - 1)
+            deletions, _G = cascade_del(deletions, _G, rml)
+            now_l_count = leaves(_G)
+            bar.update(l_count-now_l_count)
+            l_count = now_l_count
+            if settings.debug:
+                print(l_count, len(_G.vs))
+        bar.close()
+        n_vertices -= deletions
+        assert n_vertices == len(_G.vs)
+
+        return _G, n_vertices
+
+def avg_br(g: Graph):
+    dgs = 0
+    for i in range(len(g.vs)):
+        if i == 0:
+            dgs += g.vs[i].degree()
+        elif g.vs[i].degree() != 1:
+            dgs += g.vs[i].degree() - 1
+    return dgs / (len(G.vs) - leaves(G))
 
 
-print("Generating...")
-while leaves(G) > settings.max_leaves:
-    rml = random.randint(1, len(G.vs) - 1)
-    G = cascade_del(G, rml)
+while True:
+    G, n_vertices = conditional_gen()
+    best_points = []
+    best_clipped = []
+    succ = False
+    print("Average Branch", avg_br(G))
+    print("Assigning...")
+    time.sleep(0.1)
+    for trial in tqdm(range(200)):
+        points = []
+        clipped = []
+        Xpos = {}
+        for i in range(n_vertices):
+            if G.vs[i].degree() == 1 and i != 0:
+                points += [random.randint(-10, 10)]
+            else:
+                points += [inf]
 
-# fig, ax = plt.subplots()
-# plot(G, layout=G.layout("tree"), target=ax)
-# plt.show()
+        G.vs["reward"] = points
 
-n_vertices -= deletions
-
-points = []
-
-for i in range(n_vertices):
-    if G.vs[i].degree() == 1 and i != 0:
-        points += [random.randint(-8, 8)]
+        layout = G.layout('tree', root=0)
+        Xpos = {k: layout[k][1] for k in range(n_vertices)}
+        alpha_beta(G, 0, [], True)
+        # print(len(clipped))
+        if len(clipped) > len(best_clipped):
+            best_points = G.vs["reward"]
+            best_clipped = clipped
+    if len(best_clipped) >= settings.min_clip:
+        clipped = best_clipped
+        points = best_points
+        G.vs["reward"] = best_points
+        if settings.debug:
+            print(clipped)
+        break
     else:
-        points += [inf]
-
-G.vs["reward"] = points
-
-layout = G.layout('tree', root=0)
-Xpos = {k: layout[k][1] for k in range(n_vertices)}
-
-print("Calculating...")
-alpha_beta(G, 0, [], True)
+        print("No available assignment sample, retry")
 
 print(f"{n_vertices} Nodes")
 if settings.show_clip_count:
@@ -310,6 +336,10 @@ def draw(tp):
         font={"size": 60}
     )
     save_path = pathlib.Path(settings.save_path)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    if save_path.is_file():
+        raise RuntimeError(f"{save_path} should be a directory")
     fig.write_image(save_path / (f"done_{sd}.jpg" if tp else f"todo_{sd}.jpg"), height=200 + 150 * settings.layers,
                     width=500 + 100 * leaves(G))
 
